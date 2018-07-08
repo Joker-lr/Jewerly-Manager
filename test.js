997a25d7d6b5759df1b0f90df53e9ebb94107255
Your Name <you@example.com>
2018-07-08 13:04:27 +0800

    First HTML: Jewerly Exibition

--- test.js
+++ test.js
@@ -4,12 +4,11 @@ var app = new Koa();
 
 //console.log(__dirname);
 app.context.view = {};
-str = require("fs").readFileSync(__dirname+"/views/01.ejs","utf8");  //先读文件
-Nm = '01'
-app.context.view.Nm = str;
+str = require("fs").readFileSync(__dirname+"/public/test.html","utf8");  //先读文件
+app.context.view.str = str;
 
 const main = async (ctx, next) => {
-    ctx.body = '<img src=\"/public/images/jz.jpg\" alt=\"Error Happens\">';
+    ctx.body = app.context.view.str;
 }
 
 app.use(main);
